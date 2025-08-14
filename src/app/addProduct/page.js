2ae6c8cf85f32b9c07c8aef5784491f00a5ef80b
Manthiramoorthy <manthiramoorthyk15@gmail.com>
2025-08-14 18:17:07 +0530

    First commit

---
+++ src/app/addProduct/page.js
@@ -0,0 +1,10 @@
+function AddProductPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Add Product Page</h1>
+      <p>This is the page where you can add new products to the e-commerce platform.</p>
+    </div>
+  );
+}
+
+export default AddProductPage;
